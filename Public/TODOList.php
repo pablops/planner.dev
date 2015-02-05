2334aaa9e601702ce1a656fa458c1675e8d38def
Pablo <pablo@Jennis-MacBook-Air.local>
2015-02-05 13:09:02 -0600

    css exercises

--- Public/TODOList.php
+++ Public/TODOList.php
@@ -1,28 +1,31 @@
+<?php
+var_dump($_GET); 
+var_dump($_POST);
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>Todo List Items</title>
+		<link rel="stylesheet" type="text/css" href="/css/style.css">
     </head>
 
     <body>
     	<h1>TODO List</h1>
 
-		<ul>
+		<ol>
     		<li>go to the store</li>
 		    <li>wash the dishes</li>
 		    <li>fix the car</li>
-		</ul>
+		</ol>
 
-		<form method="POST" action="http://requestb.in/1ed4a1c1">
+		<form method="POST" action="TODOList.php">
 			<p>
 				<label for="something">ToDo Item</label>
 				<input id="something" name="something" type="text">
 				<br>	
 				<input type="submit" value="Login">
 			</p>
-
-
-			
 
 
 		</form>	
